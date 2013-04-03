16bc28a2dda0db12abd877620afe544d7d1ffd9d
SebastianFM <sebastiansobczyk@wp.pl>
2013-04-04 01:07:36 +0200

    v0.1.2

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl.h
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl.h
@@ -84,9 +84,11 @@
 
 #define DDL_MAX_NUM_IN_INPUTFRAME_POOL          (DDL_MAX_NUM_OF_B_FRAME + 1)
 
-/* HTC_START */
-extern int mTotalErrorFrames;
-/* HTC_END */
+enum ddl_mem_area {
+	DDL_FW_MEM	= 0x0,
+	DDL_MM_MEM	= 0x1,
+	DDL_CMD_MEM	= 0x2
+};
 
 struct ddl_buf_addr{
 	u8  *virtual_base_addr;
@@ -97,6 +99,7 @@ struct ddl_buf_addr{
 	struct msm_mapped_buffer *mapped_buffer;
 	struct ion_handle *alloc_handle;
 	u32 buffer_size;
+	enum ddl_mem_area mem_type;
 };
 enum ddl_cmd_state{
 	DDL_CMD_INVALID         = 0x0,
